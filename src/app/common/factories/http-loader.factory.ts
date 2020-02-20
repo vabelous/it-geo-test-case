4289e7621ce0e5ad7d73640b7616249a4fe198ec
Владимир Белоус <mailforbva@gmail.com>
2020-02-20 14:02:58 +0500

    delete i18n module

--- src/app/common/factories/http-loader.factory.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient, 'assets/i18n/', '.json');
-}
