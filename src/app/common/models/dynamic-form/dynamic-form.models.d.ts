ea932cf8157a0f57c3e99f026f81d12af79b6256
Владимир Белоус <mailforbva@gmail.com>
2020-02-24 13:25:46 +0500

    update server data

--- src/app/common/models/dynamic-form/dynamic-form.models.d.ts
+++ src/app/common/models/dynamic-form/dynamic-form.models.d.ts
@@ -1,17 +1,38 @@
+declare interface DynamicFormModelBlockDTO {
+    id: number,
+    block_id: number,
+    block_title: string,
+    block_description: string,
+    fields: Array<number>,
+    children: Array<DynamicFormModelBlockDTO>    
+}
+
 declare interface DynamicFormBlockDTO {
     id: number,
-    title: string,
-    parent_id: number,
-    style: object
+    style: object,
 }
 
 declare interface DynamicFormBlocksDTO extends Array<DynamicFormBlockDTO> {}
 
+declare type DynamicFormFieldAutocompleteDTO = 'off' | 'on';
+
+declare type DynamicFormFieldTypeDTO = 'input' | 'checkbox' | 'radio' | 'select';
+
+declare type DynamicFormInputTypeDTO = 'color' | 'date' | 'datetime-local' | 'email' | 'month' | 'number' | 'password' | 'tel' | 'text' | 'time' | 'url' | 'week';
+
 declare interface DynamicFormFieldDTO {
-    id: number,
-    title: string,
-    block_id: number,
-    style: string
+    id : number,
+    fieldType : DynamicFormFieldTypeDTO,
+    inputType? : DynamicFormInputTypeDTO,
+    label? : string,
+    disabled? : boolean,
+    autocomplete? : DynamicFormFieldAutocompleteDTO,
+    defaultValue? : string | number | boolean,
+    placeholder? : string,
+    tooltip? : string,
+    required? : boolean
+    mask? : string,
+    style?: string
 }
 
 declare interface DynamicFormFieldsDTO extends Array<DynamicFormFieldDTO> {}
